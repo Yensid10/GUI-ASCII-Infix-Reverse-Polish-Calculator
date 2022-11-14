0d56be8eb72086b880c03bdf6ecc7c7d669da37a
bensh <ben.shearlock@gmail.com>
2022-11-14 00:29:22 +0000

    Last finalising commit before creating first feature branch

--- Calculator/target/site/apidocs/package-search-index.js
+++ Calculator/target/site/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"uk.ac.rhul.cs2800"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"uk.ac.rhul.cs2800"}];updateSearchResults();
